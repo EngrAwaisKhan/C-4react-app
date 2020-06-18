44900e1120b2dd51cdb2780a7a614a8db461cbad
Muhammad Awais Khan <66188894+EngrAwaisKhan@users.noreply.github.com>
2020-06-18 08:36:54 -0700

    wrap into one div

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ function App() {
   let [count, setCount] = useState(0)
   let[isMorning, setMorning] = useState(false)
   return (
-    <h1>PIAIC BootCamp2020 Class-4 </h1>
-    <div className = {`box ${isMorning ? 'dayLight' : 'night'}`} >
-      <h3>Day time: {isMorning ? 'Morning' : 'Night'}</h3>
-      <Message counter = {count}/>
-      <br/>
-      <button onClick = {()=> setCount(++count)}>Update Counter</button>
-      <button onClick = {()=> setCount(0)}>Reset Counter</button>
-      <button onClick = {()=> setMorning(!isMorning)}>Change background</button>
+    <div>
+      <h1>PIAIC BootCamp2020 Class-4 </h1>
+      <div className = {`box ${isMorning ? 'dayLight' : 'night'}`} >
+        <h3>Day time: {isMorning ? 'Morning' : 'Night'}</h3>
+        <Message counter = {count}/>
+        <br/>
+        <button onClick = {()=> setCount(++count)}>Update Counter</button>
+        <button onClick = {()=> setCount(0)}>Reset Counter</button>
+        <button onClick = {()=> setMorning(!isMorning)}>Change background</button>
+      </div>
     </div>
   );
 }
